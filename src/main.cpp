2d52562bbea093056844c19a431d0c3d334a8199
mark-1-10 <mark.eller@bluewin.ch>
2025-03-05 15:25:18 +0100

    2

--- src/main.cpp
+++ src/main.cpp
@@ -43,22 +43,22 @@ int main()
     Servo servo_D1(PB_D1);
 
 
-/* 
+
     // minimal pulse width and maximal pulse width obtained from the servo calibration process
     // futuba S3001
     float servo_D0_ang_min = 0.0150f; // carefull, these values might differ from servo to servo
     float servo_D0_ang_max = 0.1150f;
     // reely S0090
-    float servo_D1_ang_min = 0.0325f;
-    float servo_D1_ang_max = 0.1175f;
- */
+    float servo_D1_ang_min = 0.0350f;
+    float servo_D1_ang_max = 0.118f;
 
-/* 
+
+
     // servo.setPulseWidth: before calibration (0,1) -> (min pwm, max pwm)
     // servo.setPulseWidth: after calibration (0,1) -> (servo_D0_ang_min, servo_D0_ang_max)
     servo_D0.calibratePulseMinMax(servo_D0_ang_min, servo_D0_ang_max);
     servo_D1.calibratePulseMinMax(servo_D1_ang_min, servo_D1_ang_max);
- */
+
 
 
     // default acceleration of the servo motion profile is 1.0e6f
@@ -69,7 +69,7 @@ int main()
     float servo_input = 0.0f;
     int servo_counter = 0; // define servo counter, this is an additional variable
                            // used to command the servo
-    const int loops_per_seconds = static_cast<int>(ceilf(1.0f / (0.001f * static_cast<float>(main_task_period_ms))));
+    const int loops_per_seconds = static_cast<int>(ceilf(0.50f / (0.001f * static_cast<float>(main_task_period_ms))));
 
     // start timer
     main_task_timer.start();
@@ -97,7 +97,7 @@ int main()
             if ((servo_input < 1.0f) &&                     // constrain servo_input to be < 1.0f
                 (servo_counter % loops_per_seconds == 0) && // true if servo_counter is a multiple of loops_per_second
                 (servo_counter != 0))                       // avoid servo_counter = 0
-                servo_input += 0.005f;
+                servo_input += 0.001f;
             servo_counter++;
         } else {
             // the following code block gets executed only once
